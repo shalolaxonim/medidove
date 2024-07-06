2cffa7b42c130a1c631e1cef9a375de6e74c489f
shalolaxon <shalolaxon72@gmail.com>
2024-07-06 11:27:00 +0500

    'starterPart'

---
+++ src/components/partOne/index.jsx
@@ -0,0 +1,21 @@
+export default function PartOne() {
+  return (
+    <div>
+      <div className="flex bg-blue-50 justify-between px-[250px] text-gray-500">
+        <div className="flex gap-[50px] py-[20px] items-center">
+          <div className="flex gap-[10px] items-center">
+            <i class="fa-solid fa-phone-flip"></i>
+            <p>+1 800 833 9780</p>
+          </div>
+          <div className="flex gap-[10px] items-center">
+            <i class="fa-solid fa-envelope"></i>
+            <p>info@example.com</p>
+          </div>
+        </div>
+        <button className="bg-[#E12454] py-[10px] px-[40px] text-white">
+          MAKE APPOINTMENT
+        </button>
+      </div>
+    </div>
+  );
+}
